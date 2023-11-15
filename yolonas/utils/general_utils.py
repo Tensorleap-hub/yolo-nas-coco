365940871613ec79233e313226be7fe6920b9164
tom Koren <tom.koren@tensorleap.ai>
2023-11-15 19:56:23 +0200

    change to od general utils and not instance semantic segmnetation

--- yolonas/utils/general_utils.py
+++ yolonas/utils/general_utils.py
@@ -4,8 +4,57 @@ from typing import Union, Optional, List, Dict
 import numpy as np
 import tensorflow as tf
 from code_loader.contract.responsedataclasses import BoundingBox
+from code_loader.helpers.detection.utils import xyxy_to_xywh_format, xywh_to_xyxy_format
+from code_loader.helpers.detection.yolo.utils import reshape_output_list
 from numpy._typing import NDArray
 from yolonas.config import CONFIG
+from yolonas.yolo_helpers.yolo_utils import DECODER
+
+
+def get_predict_bbox_list(data: tf.Tensor) -> List[BoundingBox]:
+    """
+    Description: This function takes a TensorFlow tensor data as input and returns a list of bounding boxes representing predicted annotations.
+    Input: data (tf.Tensor): A TensorFlow tensor representing the output data.
+    Output: bb_object (List[BoundingBox]): A list of bounding box objects representing the predicted annotations.
+    """
+    from_logits = True if CONFIG['MODEL_FORMAT'] != "inference" else False
+    decoded = False if CONFIG['MODEL_FORMAT'] != "inference" else True
+    class_list_reshaped, loc_list_reshaped = reshape_output_list(
+        np.reshape(data, (1, *data.shape)), decoded=decoded, image_size=CONFIG['IMAGE_SIZE'])
+    # add batch
+    #TODO
+    outputs = DECODER(loc_data=[reg_fixed], conf_data=[cls], prior_data=[None],
+              from_logits=False, decoded=True)
+    bb_object = bb_array_to_object(outputs[0], iscornercoded=True, bg_label=CONFIG['BACKGROUND_LABEL'])
+    return bb_object
+
+
+def bb_array_to_object(bb_array: Union[NDArray[float], tf.Tensor], iscornercoded: bool = True, bg_label: int = 0,
+                       is_gt=False) -> List[BoundingBox]:
+    """
+    Assumes a (X,Y,W,H) Format for the BB text
+    bb_array is (CLASSES,TOP_K,PROPERTIES) WHERE PROPERTIES =(conf,xmin,ymin,xmax,ymax)
+    """
+    bb_list = []
+    if not isinstance(bb_array, np.ndarray):
+        bb_array = np.array(bb_array)
+    # fig, ax = plt.subplots(figsize=(6, 9)
+    if len(bb_array.shape) == 3:
+        bb_array = bb_array.reshape(-1, bb_array.shape[-1])
+    for i in range(bb_array.shape[0]):
+        if bb_array[i][-1] != bg_label:
+            if iscornercoded:
+                x, y, w, h = xyxy_to_xywh_format(bb_array[i][1:5])
+                # unormalize to image dimensions
+            else:
+                x, y = bb_array[i][0], bb_array[i][1]
+                w, h = bb_array[i][2], bb_array[i][3]
+            conf = 1 if is_gt else bb_array[i][0]
+            curr_bb = BoundingBox(x=x, y=y, width=w, height=h, confidence=conf,
+                                  label=CONFIG['CATEGORIES'][int(bb_array[i][min(5, len(bb_array[i]) - 1)])])
+
+            bb_list.append(curr_bb)
+    return bb_list
 
 
 def polygon_to_bbox(vertices):
@@ -39,47 +88,78 @@ def polygon_to_bbox(vertices):
     return bbox
 
 
-def count_obj_masks_occlusions(masks: Union[np.ndarray, None], occlusion_threshold: float) -> int:
+def calculate_iou_all_pairs(bboxes: np.ndarray, image_size: int) -> np.ndarray:
     """
-    Counts the occluded masks based on Intersection over Union (IOU).
+    Calculates the Intersection over Union (IOU) for all pairs of bounding boxes.
+
+    This function utilizes vectorization to efficiently compute the IOU for all possible pairs of bounding boxes.
+    By leveraging NumPy's array operations, the calculations are performed in parallel, leading to improved performance.
 
     Args:
-        masks (Union[np.ndarray, None]): Masks represented as a NumPy array. Can be None if no masks are provided.
-        occlusion_threshold (float): Threshold value for determining occlusion based on IOU.
+        bboxes (np.ndarray): Array of bounding boxes in the format [x, y, w, h].
+        image_size (int): Size of the image.
 
     Returns:
-        int: Number of occluded masks based on the specified occlusion threshold.
-
+        np.ndarray: Array containing the IOU values for all pairs of bounding boxes.
     """
 
-    if masks is None:
-        return 0
+    # Reformat all bboxes to (x_min, y_min, x_max, y_max)
+    bboxes = np.asarray([xywh_to_xyxy_format(bbox[:-1]) for bbox in bboxes]) * image_size
+    num_bboxes = len(bboxes)
+    # Calculate coordinates for all pairs
+    x_min = np.maximum(bboxes[:, 0][:, np.newaxis], bboxes[:, 0])
+    y_min = np.maximum(bboxes[:, 1][:, np.newaxis], bboxes[:, 1])
+    x_max = np.minimum(bboxes[:, 2][:, np.newaxis], bboxes[:, 2])
+    y_max = np.minimum(bboxes[:, 3][:, np.newaxis], bboxes[:, 3])
 
-    if masks[0, ...].shape != CONFIG["IMAGE_SIZE"]:
-        masks = tf.image.resize(masks[..., None], CONFIG["IMAGE_SIZE"], tf.image.ResizeMethod.NEAREST_NEIGHBOR)[..., 0]
-        masks = masks.numpy()
+    # Calculate areas for all pairs
+    areas = (bboxes[:, 2] - bboxes[:, 0]) * (bboxes[:, 3] - bboxes[:, 1])
 
-    num_masks = len(masks)
+    # Calculate intersection area for all pairs
+    intersection_area = np.maximum(x_max - x_min, 0) * np.maximum(y_max - y_min, 0)
 
-    # Reshape masks to have a third dimension
-    masks = np.expand_dims(masks, axis=-1)
+    # Calculate union area for all pairs
+    union_area = areas[:, np.newaxis] + areas - intersection_area
 
-    # Create tiled versions of masks for element-wise addition
-    tiled_masks = np.broadcast_to(masks, (num_masks, num_masks, masks.shape[1], masks.shape[2], 1))
-    tiled_masks_transposed = np.transpose(tiled_masks, axes=(1, 0, 2, 3, 4))
+    # Calculate IOU for all pairs
+    iou = intersection_area / union_area
+    iou = iou[np.triu_indices(num_bboxes, k=1)]
+    return iou
 
-    # Compute overlay matrix
-    overlay = tiled_masks + tiled_masks_transposed
 
-    # Exclude same mask occlusions and duplicate pairs
-    mask_indices = np.triu_indices(num_masks, k=1)
-    overlay = overlay[mask_indices]
+def count_obj_bbox_occlusions(img: np.ndarray, bboxes: np.ndarray, occlusion_threshold: float, calc_avg_flag: bool) -> \
+        Union[float, int]:
+    """
+    Counts the occluded bounding boxes of a specific object category in an image.
 
-    intersection = np.sum(overlay > 1, axis=(-1, -2, -3))
-    union = np.sum(overlay > 0, axis=(-1, -2, -3))
+    This function takes an image and an array of bounding boxes as input and counts the number of occluded
+    bounding boxes of a specific object category. The occlusion is determined based on the Intersection over Union (IOU)
+    between the bounding boxes.
 
-    iou = intersection / union
-    return int(np.sum(iou > occlusion_threshold))
+    Args:
+        img (np.ndarray): Image represented as a NumPy array.
+        bboxes (np.ndarray): Array of bounding boxes in the format [x, y, w, h, label].
+        occlusion_threshold (float): Threshold value for determining occlusion based on IOU.
+        calc_avg_flag (bool): Flag indicating whether to calculate the average occlusion count.
+
+    Returns:
+        Union[float, int]: Number of occluded bounding boxes of the specified object category.
+                           If calc_avg_flag is True, it returns the average occlusion count as a float.
+                           If calc_avg_flag is False, it returns the total occlusion count as an integer.
+
+    """
+    img_size = img.shape[0]
+    label = CONFIG["CATEGORIES"].index('object')
+    obj_bbox = bboxes[bboxes[..., -1] == label]
+    if len(obj_bbox) == 0:
+        return 0.0
+    else:
+        ious = calculate_iou_all_pairs(obj_bbox, img_size)
+        occlusion_count = len(ious[ious > occlusion_threshold])
+        if calc_avg_flag:
+            return int(occlusion_count / len(obj_bbox))
+        else:
+            return occlusion_count
 
 
 def remove_label_from_bbs(bbs_object_array, removal_label, add_to_label):
@@ -109,54 +189,21 @@ def calculate_overlap(box1, box2):
     return overlap_area
 
 
-def get_argmax_map_and_separate_masks(image, bbs, masks):
-    image_size = image.shape[:2]
-    argmax_map = np.zeros(image_size, dtype=np.uint8)
-    cats_dict = {}
-    separate_masks = []
-    for bb, mask in zip(bbs, masks):
-        if mask.shape != image_size:
-            resize_mask = tf.image.resize(mask[..., None], image_size, tf.image.ResizeMethod.NEAREST_NEIGHBOR)[..., 0]
-            if not isinstance(resize_mask, np.ndarray):
-                resize_mask = resize_mask.numpy()
-        else:
-            resize_mask = mask
-        resize_mask = resize_mask.astype(bool)
-        label = bb.label
-        instance_number = cats_dict.get(label, 0)
-        # update counter if reach max instances we treat the last objects as one
-        cats_dict[label] = instance_number + 1 if instance_number < CONFIG[
-            "MAX_INSTANCES_PER_CLASS"] else instance_number
-        label_index = CONFIG["CATEGORIES"].index(label) * CONFIG["MAX_INSTANCES_PER_CLASS"] + cats_dict[label]
-        if label == 'Tote':
-            empty = argmax_map == 0
-            tote = (argmax_map >= CONFIG["CATEGORIES"].index(label) * CONFIG["MAX_INSTANCES_PER_CLASS"]) & \
-                   (argmax_map < CONFIG["CATEGORIES"].index(label) * (CONFIG["MAX_INSTANCES_PER_CLASS"] + 1))
-            argmax_map[(empty | tote) & resize_mask] = label_index
-        else:
-            argmax_map[resize_mask] = label_index
-        if bb.label == 'Object':
-            separate_masks.append(resize_mask)
-    argmax_map[argmax_map == 0] = len(CONFIG['INSTANCES']) + 1
-    argmax_map -= 1
-    return {"argmax_map": argmax_map, "separate_masks": separate_masks}
-
-
-def extract_and_cache_bboxes(idx: int, data: Dict):
-    x = data['samples'][idx]
-    coco = data['cocofile']
-    ann_ids = coco.getAnnIds(imgIds=x['id'])
-    anns = coco.loadAnns(ann_ids)
+def extract_bboxes_yolo(path: str):
     bboxes = np.zeros([CONFIG['MAX_BB_PER_IMAGE'], 5])
-    max_anns = min(CONFIG['MAX_BB_PER_IMAGE'], len(anns))
-    for i in range(max_anns):
-        ann = anns[i]
-        if isinstance(ann['segmentation'], list) and ann['category_id'] <= CONFIG['CLASSES']:
-            img_size = (x['height'], x['width'])
-            class_id = ann['category_id']
-            bbox = polygon_to_bbox(ann['segmentation'][0])
-            bbox /= np.array((img_size[1], img_size[0], img_size[1], img_size[0]))
-            bboxes[i, :4] = bbox
-            bboxes[i, 4] = class_id
+    # Load bounding box annotations from the YOLO format
+    # You'll need to parse the YOLO annotation file to get bounding boxes and class IDs
+    # Assuming YOLO annotation format as: "<class_id> <center_x> <center_y> <width> <height>"
+    with open(path, 'r') as file:
+        lines = file.readlines()
+
+    max_anns = min(CONFIG['MAX_BB_PER_IMAGE'], len(lines))
+    for i, line in enumerate(lines):
+        line = line.strip().split()
+        class_id = int(line[0])
+        bbox = list(map(float, line[1:]))
+        bboxes[i, :4] = bbox
+        bboxes[i, 4] = class_id
+
     bboxes[max_anns:, 4] = CONFIG['BACKGROUND_LABEL']
     return bboxes
