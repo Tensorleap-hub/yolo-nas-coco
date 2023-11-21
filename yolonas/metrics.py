a1f9abd89e01bb18fb4efc2defd1251ce11d53aa
ranhomri <ran.homri@tensorleap.ai>
2023-11-21 13:12:55 +0200

    fixed loss

--- yolonas/metrics.py
+++ yolonas/metrics.py
@@ -13,30 +13,33 @@ def custom_yolo_nas_loss(y_true, reg: tf.Tensor, cls: tf.Tensor):
                   prior_data=[None],
                   from_logits=False,
                   decoded=True)
-    y_pred_lst = [tf.convert_to_tensor(bb) for bb in res if bb.size != 0]
-    if len(res) == 0:
-        return tf.convert_to_tensor([0])
+    if len([bb for bb in res if bb.size != 0]) == 0:
+        return tf.convert_to_tensor([0.0])
 
     regression_loss = []
-    for y_pred in y_pred_lst:
-        # Extract confidence, coordinates, and class predictions
-        pred_confidence = y_pred[:, 0]
-        pred_boxes = y_pred[:, 1:5]
-        pred_class_probs = y_pred[:, 5:]
+    y_pred_lst = [tf.convert_to_tensor(bb) for bb in res]
+    for i, y_pred in enumerate(y_pred_lst):
+        if y_pred.shape != 0:
+            # Extract confidence, coordinates, and class predictions
+            pred_confidence = y_pred[:, 0]
+            pred_boxes = y_pred[:, 1:5]
+            pred_class_probs = y_pred[:, 5:]
 
-        true_boxes = y_true[:, :, :4]
-        true_class_probs = y_true[:, :, 4:]
+            true_boxes = y_true[i, :, :4]
+            true_class_probs = y_true[i, :, 4:]
 
-        # Calculate IoU for each pair of predicted and true bounding boxes
-        pred_boxes = tf.cast(tf.expand_dims(pred_boxes, axis=1), tf.float32)  # Shape: (n_pred_boxes, 1, 4)
-        true_boxes = tf.cast(tf.expand_dims(true_boxes, axis=0), tf.float32)  # Shape: (1, n_true_boxes, 4)
+            # Calculate IoU for each pair of predicted and true bounding boxes
+            pred_boxes = tf.cast(tf.expand_dims(pred_boxes, axis=1), tf.float32)  # Shape: (n_pred_boxes, 1, 4)
+            true_boxes = tf.cast(tf.expand_dims(true_boxes, axis=0), tf.float32)  # Shape: (1, n_true_boxes, 4)
 
-        mask = tf.cast(y_true[:, :, 4] != CONFIG['BACKGROUND_LABEL'], tf.float32)
-        mask_expanded = tf.expand_dims(mask, axis=-1)
+            mask = tf.cast(tf.expand_dims(y_true[i, :, 4], 0) != CONFIG['BACKGROUND_LABEL'], tf.float32)
+            mask_expanded = tf.expand_dims(mask, axis=-1)
 
-        regression_loss.append(tf.keras.losses.Huber()(true_boxes * mask_expanded, pred_boxes * mask_expanded))
+            regression_loss.append(tf.keras.losses.Huber()(true_boxes * mask_expanded, pred_boxes * mask_expanded))
+        else:
+            regression_loss.append(tf.convert_to_tensor(0.0))
 
-    return tf.truediv(regression_loss, len(res))
+    return tf.convert_to_tensor(regression_loss)
 
 
 def placeholder_loss(y_true, reg: tf.Tensor, cls: tf.Tensor) -> tf.Tensor:  # return batch
