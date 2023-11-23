859a33d54c022fb194f442ba72d27caead3aebdb
yotamazriel <yotam@tensorleap.ai>
2023-11-23 18:26:20 +0200

    read all images

--- yolonas/data/preprocessing.py
+++ yolonas/data/preprocessing.py
@@ -8,19 +8,9 @@ from yolonas.config import CONFIG
 from yolonas.utils.dataclasses import RescaleMetadata, PaddingCoordinates
 
 
-def load_set(coco, local_filepath, load_union=False):
-    # get all images containing given categories
-    CATEGORIES = []
-    catIds = coco.getCatIds(CATEGORIES)  # Fetch class IDs only corresponding to the Classes
-    if not load_union:
-        imgIds = coco.getImgIds(catIds=catIds)  # Get all images containing the Category IDs together
-    else:  # get images contains any of the classes
-        imgIds = set()
-        for cat_id in catIds:
-            image_ids = coco.getImgIds(catIds=[cat_id])
-            imgIds.update(image_ids)
-        imgIds = list(imgIds)
-    imgs = coco.loadImgs(imgIds)
+def load_set(coco, local_filepath):
+    imgIds = coco.getImgIds()  # Get IDs for all images
+    imgs = coco.loadImgs(imgIds)  # Load all images
     return imgs
 
 
